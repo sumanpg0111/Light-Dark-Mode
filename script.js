36b520b89e59adfed1239fcb0f3b4a08cfab462f
sumanpg0111 <123824399+sumanpg0111@users.noreply.github.com>
2024-10-06 10:32:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const fullDarkModeCheckbox = document.querySelector('#full-dark-mode')
+const containedDarkModeCheckbox = document.querySelector('#contained-dark-mode')
+const container = document.querySelector('.container')
+
+const isFullDarkMode = JSON.parse(localStorage.getItem('fullDarkModeCheckbox'))
+const isContainedDarkMode = JSON.parse(localStorage.getItem('containedDarkModeCheckbox'))
+
+if(isFullDarkMode){
+    fullDarkModeCheckbox.checked = true
+}
+if(isContainedDarkMode){
+    containedDarkModeCheckbox.checked = true
+}
+
+changeFullDarkMode()
+changeContainedDarkMode()
+
+fullDarkModeCheckbox.addEventListener('change',() => {
+    changeFullDarkMode()
+    changeContainedDarkMode()
+
+})
+
+function changeFullDarkMode(){
+    changeTheme(fullDarkModeCheckbox , document.body , 'fullDarkModeCheckbox')
+}
+
+function changeContainedDarkMode(){
+    changeTheme(containedDarkModeCheckbox, container , 'containedDarkModeCheckbox')
+}
+
+function changeTheme(checkbox, element, LocalStorageKey){
+    if(checkbox.checked){
+        element.classList.add('dark')
+        localStorage.setItem(LocalStorageKey, true)
+    }else{
+        element.classList.remove('dark')
+        localStorage.setItem(LocalStorageKey, false)
+    }
+}
+
+containedDarkModeCheckbox.addEventListener('change',changeContainedDarkMode)
